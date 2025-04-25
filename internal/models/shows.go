5add2e1feb274b1de07e3d56700d2975197c61a7
kehl-gopher <kelanidarasimi9@gmail.com>
2025-04-25 17:53:36 +0100

    chore: define custom json type for show time

--- internal/models/shows.go
+++ internal/models/shows.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -43,3 +45,24 @@ func (s *ShowTime) Scan(value interface{}) error {
 	return fmt.Errorf("failed to scan ShowTime: %v", value)
 }
 
+func (s ShowTime) Value() (driver.Value, error) {
+	return time.Time(s).Format("15:04:05"), nil
+}
+
+func (s ShowTime) MarshalJSON() ([]byte, error) {
+	t := time.Time(s)
+	return json.Marshal(t.Format("15:04:05"))
+}
+
+func (s *ShowTime) UnmarshalJSON(data []byte) error {
+	var t time.Time
+	if err := json.Unmarshal(data, &t); err != nil {
+		return err
+	}
+	t, err := time.Parse("15:04:05", t.Format("15:04:05"))
+	if err != nil {
+		return err
+	}
+	*s = ShowTime(t)
+	return nil
+}
