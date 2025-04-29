5dfd9be6f0d532f82e5486ee842b2d0513e94ae6
kehl-gopher <kelanidarasimi9@gmail.com>
2025-04-29 15:51:38 +0100

    chore: secure the showroutes for admin only

--- pkg/router/shows.go
+++ pkg/router/shows.go
@@ -7,12 +7,13 @@ import (
 	"github.com/kehl-gopher/movie-seat-reservation-theatre/internal/env"
 	"github.com/kehl-gopher/movie-seat-reservation-theatre/internal/repository"
 	"github.com/kehl-gopher/movie-seat-reservation-theatre/pkg/controller/shows"
+	"github.com/kehl-gopher/movie-seat-reservation-theatre/pkg/middleware"
 )
 
 func ShowRoutes(r *gin.Engine, db *repository.Database, config *env.Config) {
 
 	showBase := shows.ShowBase{DB: db, Config: config}
-	showRoutes := r.Group(fmt.Sprintf("%s/show", config.BASEURL))
+	showRoutes := r.Group(fmt.Sprintf("%s/show", config.BASEURL), middleware.AuthMiddleWare(config.SECRET_KEY, db), middleware.AuthAdmin())
 	{
 		showRoutes.POST("/", showBase.CreateShows)
 	}
